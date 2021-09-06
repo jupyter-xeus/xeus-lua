e505299f4bf24587aaf205169ebfc373418b8051
DerThorsten <derthorstenbeier@gmail.com>
2021-09-06 16:37:57 +0200

    using exports

--- include/xeus-lua/xeus_lua_config.hpp
+++ include/xeus-lua/xeus_lua_config.hpp
@@ -14,7 +14,7 @@
 // Project version
 #define XLUA_VERSION_MAJOR 0
 #define XLUA_VERSION_MINOR 1
-#define XLUA_VERSION_PATCH 10
+#define XLUA_VERSION_PATCH 11
 
 // Composing the version string from major, minor and patch
 #define XLUA_CONCATENATE(A, B) XLUA_CONCATENATE_IMPL(A, B)
