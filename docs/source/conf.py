4d30b279465cef68c17ef86ed6d3df533f7aa407
DerThorsten <derthorstenbeier@gmail.com>
2021-09-07 11:16:27 +0200

    changed readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ import subprocess
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if on_rtd:
-    subprocess.call('cd ..; doxygen', shell=True)
+# if on_rtd:
+#     subprocess.call('cd ..; doxygen', shell=True)
 
 import sphinx_rtd_theme
 
@@ -18,8 +18,10 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 def setup(app):
     app.add_css_file("main_stylesheet.css")
 
-extensions = ['breathe']
-breathe_projects = { 'xeus-lua': '../xml' }
+extensions = [
+#'breathe'
+]
+# breathe_projects = { 'xeus-lua': '../xml' }
 templates_path = ['_templates']
 html_static_path = ['_static']
 source_suffix = '.rst'
@@ -31,7 +33,7 @@ author = 'Thorsten Beier'
 html_logo = 'quantstack-white.svg'
 
 exclude_patterns = []
-highlight_language = 'c++'
+highlight_language = 'Lua'
 pygments_style = 'sphinx'
 todo_include_todos = False
 htmlhelp_basename = 'xeus-luadoc'
