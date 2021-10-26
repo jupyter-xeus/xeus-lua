ddbca20b4b2e7c7b992e1c849492e0d422325d36
martinRenou <martin.renou@gmail.com>
2021-10-26 18:08:27 +0200

    Rest of the properties

--- src/xcanvas.cpp
+++ src/xcanvas.cpp
@@ -71,27 +71,6 @@ void setup_xcanvas(
     // Simple methods
     canvas_lua_type["display"] = &xwidgets_type::display;
 
-    XLUA_ADD_PROPERTY(canvas_lua_type, int, width);
-    XLUA_ADD_PROPERTY(canvas_lua_type, int, height);
-    XLUA_ADD_PROPERTY(canvas_lua_type, bool, sync_image_data);
-    XLUA_ADD_PROPERTY(canvas_lua_type, double, global_alpha);
-    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, font);
-    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, text_align);
-    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, text_baseline);
-    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, direction);
-    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, global_composite_operation);
-    XLUA_ADD_PROPERTY(canvas_lua_type, double, shadow_offset_x);
-    XLUA_ADD_PROPERTY(canvas_lua_type, double, shadow_offset_y);
-    XLUA_ADD_PROPERTY(canvas_lua_type, double, shadow_blur);
-    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, shadow_color);
-    XLUA_ADD_PROPERTY(canvas_lua_type, double, line_width);
-    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, line_cap);
-    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, line_join);
-    XLUA_ADD_PROPERTY(canvas_lua_type, double, miter_limit);
-    XLUA_ADD_PROPERTY(canvas_lua_type, double, line_dash_offset);
-    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, fill_style);
-    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, stroke_style);
-
     // Rect methods
     canvas_lua_type["fill_rect"] = sol::overload(
         [](xwidgets_type & self,  double x, double y, double width){
@@ -157,10 +136,7 @@ void setup_xcanvas(
 
     detail_table[widget_name] = canvas_lua_type;
 
-
-
     // add properties
-    XLUA_ADD_PROPERTY(canvas_lua_type, bool, _send_client_ready_event);
     XLUA_ADD_PROPERTY(canvas_lua_type, int, width);
     XLUA_ADD_PROPERTY(canvas_lua_type, int, height);
     XLUA_ADD_PROPERTY(canvas_lua_type, bool, sync_image_data);
@@ -170,15 +146,20 @@ void setup_xcanvas(
     XLUA_ADD_PROPERTY(canvas_lua_type, std::string, text_align);
     XLUA_ADD_PROPERTY(canvas_lua_type, std::string, text_baseline);
     XLUA_ADD_PROPERTY(canvas_lua_type, std::string, direction);
-    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, global_composite_operation);    
+    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, global_composite_operation);
 
     XLUA_ADD_PROPERTY(canvas_lua_type, double, shadow_offset_x);
     XLUA_ADD_PROPERTY(canvas_lua_type, double, shadow_offset_y);
     XLUA_ADD_PROPERTY(canvas_lua_type, double, shadow_blur);
-    //MISSING  shadow_color
+    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, shadow_color);
+
+    XLUA_ADD_PROPERTY(canvas_lua_type, double, line_width);
     XLUA_ADD_PROPERTY(canvas_lua_type, std::string, line_cap);
     XLUA_ADD_PROPERTY(canvas_lua_type, std::string, line_join);
-
+    XLUA_ADD_PROPERTY(canvas_lua_type, double, miter_limit);
+    XLUA_ADD_PROPERTY(canvas_lua_type, double, line_dash_offset);
+    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, fill_style);
+    XLUA_ADD_PROPERTY(canvas_lua_type, std::string, stroke_style);
 
     // patch the print function with pure lua code
     std::string script = R""""(
@@ -195,7 +176,7 @@ void setup_xcanvas(
     end
 
     for k,widget_cls in pairs(  atomic_widgets) do
-    ilua.canvas[k:sub(2)] = function(options) 
+    ilua.canvas[k:sub(2)] = function(options)
             options = options or {}
             w = widget_cls.new()
             for kw,val in pairs(options) do
