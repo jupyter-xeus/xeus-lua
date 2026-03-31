87b0d0c75bf858f6727f1f568e1c798686b6adb9
DerThorsten <derthorstenbeier@gmail.com>
2026-03-31 16:10:46 +0200

    typo

--- test/test_xeus_lua_kernel.py
+++ test/test_xeus_lua_kernel.py
@@ -27,7 +27,7 @@ class XeusLuaTests(jupyter_kernel_test.KernelTests):
     ]
 
     complete_code_samples = ["print('hello, world')", "function foo() function bar() end end"]
-    incomplete_code_samples = ["function foo(""]
+    incomplete_code_samples = ["function foo("]
 
     def test_xeus_lua_io_write(self):
         self.flush_channels()
